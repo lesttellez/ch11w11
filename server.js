716b2d2522b9bce367d75e30f3edf46ec9ba9a81
Celeste T <celestet@Celestes-MacBook-Air.local>
2022-08-22 12:13:40 -0700

    setting up some files

---
+++ server.js
@@ -0,0 +1,21 @@
+// Dependencies
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+// Sets up the Express app to handle data parsing
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(apiRoutes);
+app.use(htmlRoutes);
+
+// Starts the server to begin listening
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
